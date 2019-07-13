17874e6703478ff720919572f9d7aea3faff9fc8
kumayk <kumayk1882@gmail.com>
2019-07-13 14:41:17 +0900

    Add admin controller action

---
+++ app/controllers/admin_items_controller.rb
@@ -0,0 +1,21 @@
+class AdminItemsController < ApplicationController
+
+def index
+end
+
+def show
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+end
+
+end
--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -1,2 +1,15 @@
 class AdminUsersController < ApplicationController
+
+def show
+end
+
+def edit
+end
+
+def destroy
+end
+
+def update
+end
+
 end
