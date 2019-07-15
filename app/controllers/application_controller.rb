a209948ee09408cbdcc8d4b1f090284c14cd0a8f
kuma47 <t.y.baskeno.3@gmail.com>
2019-07-15 16:40:52 +0900

    f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
+
+  def set_search
+	@search = Item.ransack(params[:id])
+  	@result = @search.result
+  end
+
+  private
+  def  search_params
+  	params.require(:search).permit(:disk_name, :disk_image, :stock, :price, :item_status)
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,5 @@ class ItemsController < ApplicationController
 
   def show
   end
+  
 end
