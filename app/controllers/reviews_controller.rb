089fe3af37a23ffb4a61d93ad5069829686d2e1c
kumayk <kumayk1882@gmail.com>
2019-07-17 17:51:00 +0900

    商品詳細ページ(レビュー完成)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,6 @@ def create
     # @item = Item.find(params[:item_id])
     @review = Review.new(review_params)
     @review.user_id = current_user.id
-    # @reviews = Review,new(review_params)
     # @item.user_id = current_user.id
     # @review.user_id = current_user.id
     @review.save
@@ -18,9 +17,9 @@ def update
 end
 
 def destroy
-    @review = Review.find(params[:id])
-    if @review.destroy
-      redirect_to item_path(id: @review.item_id)
+    review = Review.find(params[:id])
+    if review.destroy
+      redirect_to item_path(id: review.item_id)
     end
 end
 
