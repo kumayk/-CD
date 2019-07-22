b0850fc8e153710b25ad0151002506d4f3574805
kuma47 <t.y.baskeno.3@gmail.com>
2019-07-22 13:17:44 +0900

    EE

--- app/controllers/cart_details_controller.rb
+++ app/controllers/cart_details_controller.rb
@@ -1,11 +1,12 @@
 class CartDetailsController < ApplicationController
 	def show
 		@cart = CartDetail.all
-		@totalprice = CartDetail.all.sum(:price)
-		@totalquantity = CartDetail.all.sum(:quantity)
+		# @totalprice = CartDetail.all.sum(:price)
+		# @totalquantity = CartDetail.all.sum(:quantity)
 	end
 
 	def create
+		cart = Cart.new
 	end
 
 	def update
