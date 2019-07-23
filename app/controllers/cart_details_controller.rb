da3988b42e73c6794f667380ece7ae265fd796bf
kuma47 <t.y.baskeno.3@gmail.com>
2019-07-23 16:57:40 +0900

    論理途中

--- app/controllers/cart_details_controller.rb
+++ app/controllers/cart_details_controller.rb
@@ -1,23 +1,25 @@
 class CartDetailsController < ApplicationController
-	before_action :setup_cart_detail!, only: [:cerate, :update]
+	before_action :setup_cart_detail!, only: [:cerate]
 
 	def index
 		@cart = CartDetail.all
 		@cart = current_user.cart_details
 	    @totalprice = 0
 	    @cart.each do |cart|
-	    	@totalprice += cart.item.price
+	    	@itemprice = cart.item.price * cart.quantity
+	    	@totalprice += @itemprice
 	    end
     end
 
 	def create
-	    @cart_item = current_user.cart_details.build(item_id: params[:cart_detail][:item_id])
+	    @cart_item = current_user.cart_details.build(cart_detail)
 	    @cart_item.save
         redirect_to cart_details_path
 	end
 
 	def update
-	    cart_item.update(quantity: params[:quantity].to_i)
+	    cart = CartDetail.find(params[:id])
+	    cart.update(cart_params)
         redirect_to cart_details_path
 	end
 
@@ -31,8 +33,15 @@ class CartDetailsController < ApplicationController
 
 	def setup_cart_item!
         @cart_item = current_user.items.find(item_id: params[:item_id])
+    end
 
     def cart_detail
       params.require(:cart_detail).permit(:quantity, :item_id)
     end
+
+    def cart_params
+      params.require(:cart_detail).permit(:quantity)
+    end
+
+
 end
