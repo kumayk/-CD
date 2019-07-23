8beb167dc219d25f7ccd8506ae223f2fe7263394
Tomoya Ogawa <ot1019ot@gmail.com>
2019-07-23 18:42:37 +0900

    merge

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,9 +6,8 @@
 #     https: false
 #   )
 # end
-<<<<<<< HEAD
+
 
 Refile.secret_key = '8e823c94117128a5b628fc052ea8f9db811e9b9f4b49564eb6dce582d1ab6cefcc50611e4f3dc9f3d5cbf82a33b40537d65ad1af832e4f1a2a681fc845b5beb2'
-=======
 Refile.secret_key = '17c7de93a0d00fba7111fec5a8c72ef542c2ae93fd4732d47d18dfbf012eb7a159826216f34c48943911fc8da5ce92d94273100755a3655ef0b864d45573cb4d'
->>>>>>> aki/master
+
