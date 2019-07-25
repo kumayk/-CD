adf14307229a676b1fa1b2fd5ffdb08deda960de
Tomoya Ogawa <ot1019ot@gmail.com>
2019-07-25 14:54:40 +0900

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -87,7 +87,7 @@ ActiveRecord::Schema.define(version: 2019_07_23_065408) do
     t.string "shipping_address"
     t.string "payment"
     t.integer "sub_total_price"
-    t.integer "shipping_fee"
+    t.integer "shipping_fee", default: 500
     t.integer "total_price"
     t.boolean "is_deleted"
     t.datetime "created_at", null: false
@@ -100,8 +100,6 @@ ActiveRecord::Schema.define(version: 2019_07_23_065408) do
     t.boolean "deleted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.integer "item_id"
     t.boolean "is_deleted"
   end
 
