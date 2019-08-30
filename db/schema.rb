ea8b703745362ff74c8ef105abcdb2b1fa461386
kumayk <kumayk1882@gmail.com>
2019-08-30 17:06:00 +0900

    finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_092819) do
+ActiveRecord::Schema.define(version: 2019_07_28_092327) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -104,8 +104,8 @@ ActiveRecord::Schema.define(version: 2019_07_28_092819) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_deleted"
-    t.integer "item_id"
-    t.integer "user_id"
+    t.string "item_id"
+    t.string "user_id"
   end
 
   create_table "shipping_addresses", force: :cascade do |t|
